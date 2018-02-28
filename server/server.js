e1991974892d4258618e1da16ea64950f42684ad
Paul Dhanda <paulvirdhanda@gmail.com>
2018-02-28 11:09:11 -0800

    adding styles

--- server/server.js
+++ server/server.js
@@ -13,9 +13,15 @@ app.use(express.static(clientPath));
 const server = http.createServer(app);
 
 const io = socketio(server);
+
 io.on('connection', (sock)  => {
+    console.log('Someone connected');
     sock.emit('message', 'Hi, you are connected');
 
+    sock.on('message', (text)  =>{
+        io.emit('message', text);
+        
+    });
 });
 
 server.on('error', (err) =>{
